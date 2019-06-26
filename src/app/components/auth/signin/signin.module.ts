55a9459276df1600665886484ce4ad6f5872ee05
Assad Shahbaz <47604036+assadshahbaz@users.noreply.github.com>
2019-06-26 18:19:05 +0500

    26-06-19

---
+++ src/app/components/auth/signin/signin.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SigninComponent } from './signin.component';
+import { SigninRoutingModule } from './signin-routing.module';
+
+@NgModule({
+  declarations: [SigninComponent],
+  imports: [
+    CommonModule,
+    SigninRoutingModule
+  ]
+})
+export class SigninModule { }
