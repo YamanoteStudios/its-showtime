e457b794b87aaf6d06418d8ad6ea72d07d1a421f
Assad Shahbaz <47604036+assadshahbaz@users.noreply.github.com>
2019-06-26 18:44:50 +0500

    26-06-19

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HeaderComponent } from './components/common/header/header.component';
 import { HomeComponent } from './components/common/home/home.component';
 import { FooterComponent } from './components/common/footer/footer.component';
 import { AppRoutingModule } from './app-routing.module';
+import { MoviesListService } from './core/services/movies-list/movies-list.service';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,9 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    MoviesListService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
